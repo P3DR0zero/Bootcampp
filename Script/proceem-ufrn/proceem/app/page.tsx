fc61f315a99572b0e330ca60ff646b27f9d1842d
strychz <ykhalloween4@gmail.com>
2025-04-24 22:09:04 -0300

    Interface login v1

---
+++ Script/proceem-ufrn/proceem/app/page.tsx
@@ -0,0 +1,13 @@
+import Typography from '@mui/material/Typography';
+import FormColaborador from "@/app/ui/forms/formcolaborador";
+
+export default function Home() {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Typography variant="h2" sx={{ textAlign: 'center' }}>
+        PROCEEM-UFRN
+      </Typography>
+      <FormColaborador />
+    </div>
+  );
+}
---
+++ Script/proceem-ufrn/proceem/app/ui/forms/components/CustomIcons.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const GoogleIcon = () => <svg>Google Icon</svg>;
