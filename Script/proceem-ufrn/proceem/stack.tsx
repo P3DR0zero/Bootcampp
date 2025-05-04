3d5159398805ed3a9c308550d266eb0738f20a60
dexx <joseemanoel360@gmail.com>
2025-05-03 22:33:52 -0300

    stack auth

--- Script/proceem-ufrn/proceem/stack.tsx
+++ Script/proceem-ufrn/proceem/stack.tsx
@@ -1,7 +1,6 @@
-import "server-only";
-
 import { StackServerApp } from "@stackframe/stack";
 
 export const stackServerApp = new StackServerApp({
-  tokenStore: "nextjs-cookie",
+  projectId: "e17ad92f-719e-4331-9154-00c4b81f6023",
+  tokenStore: "nextjs-cookie"
 });
